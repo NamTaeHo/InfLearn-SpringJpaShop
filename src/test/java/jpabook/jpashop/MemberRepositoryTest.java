db3f05cefc798b31bf89ec03d6bc4105581bd734
NamTaeHo <han127kr@gmail.com>
2024-01-08 17:01:00 +0900

    도메인 분석 설계 및 엔티티 작성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
-import org.aspectj.weaver.AjcMemberMaker;
-import org.assertj.core.api.Assertions;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,18 +25,11 @@ class MemberRepositoryTest {
         member.setUsername("memberA");
 
         Long savedId = memberRepository.save(member);
-
         Member findMember = memberRepository.find(savedId);
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
         assertThat(findMember).isSameAs(member);
-
     }
-
-
-
-
-
 }

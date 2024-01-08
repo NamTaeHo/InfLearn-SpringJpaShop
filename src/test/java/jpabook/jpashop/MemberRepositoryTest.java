eb6a420b8d60e1477141263b600ab36bc4056f79
NamTaeHo <han127kr@gmail.com>
2024-01-08 18:02:15 +0900

    회원 repository 개발

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -17,19 +17,22 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
+
     @Test
     @Transactional
     @Rollback(false)
     public void testMember() throws Exception{
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
         assertThat(findMember).isEqualTo(member);
         assertThat(findMember).isSameAs(member);
     }
+
+
 }

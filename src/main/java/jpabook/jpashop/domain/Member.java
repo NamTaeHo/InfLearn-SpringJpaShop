59c54608e38970713305e676ad1271ed0b32a825
NamTaeHo <han127kr@gmail.com>
2024-01-16 22:23:40 +0900

    회원 가입, 수정, 조회 api 제작

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ public class Member {
     @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
     private String name;
 
     @Embedded
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -51,4 +51,12 @@ public class MemberService {
       }
 
 
+
+      @Transactional
+    public void update(Long id, String name) {
+
+          Member member = memberRepository.findOne(id);
+          member.setName(name);
+
+      }
 }
